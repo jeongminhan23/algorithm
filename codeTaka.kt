a3d5a962bd86de3950b54c940dc33a9e9c2f3357
jeongminhan23 <149580488+jeongminhan23@users.noreply.github.com>
2023-12-04 16:05:48 +0900

    Add files via upload

---
+++ codeTaka.kt
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = num1 - num2
+        return answer
+    }
+}"
