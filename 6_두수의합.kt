4ae4cd2aa54153b0fe3e4931bd735f05ba7b6ae8
jeongminhan23 <149580488+jeongminhan23@users.noreply.github.com>
2023-12-04 16:16:56 +0900

    Update and rename codeTaka.kt to 6_두수의합.kt

--- 6_두수의합.kt
+++ 6_두수의합.kt
@@ -1,4 +1,4 @@
-https://school.programmers.co.kr/learn/courses/30/lessons/120802
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
 
 class Solution {
     fun solution(num1: Int, num2: Int): Int {
