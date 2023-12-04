d675896105bc5a1755aebb12660136f585a33e0f
jeongminhan23 <149580488+jeongminhan23@users.noreply.github.com>
2023-12-04 16:12:01 +0900

    Add files via upload

---
+++ codeTaka.kt
@@ -0,0 +1,8 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+class Solution {
+    fun solution(age: Int): Int {
+        var answer: Int = 2023 - age
+        return answer
+    }
+}
