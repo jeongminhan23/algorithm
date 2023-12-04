24165128c829f2387431d58106eff8f5a5205b2d
jeongminhan23 <149580488+jeongminhan23@users.noreply.github.com>
2023-12-04 16:13:12 +0900

    Update 4_나이출력.kt

--- 4_나이출력.kt
+++ 4_나이출력.kt
@@ -1,4 +1,4 @@
-https://school.programmers.co.kr/learn/courses/30/lessons/120805
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805
 
 class Solution {
     fun solution(age: Int): Int {
